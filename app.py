a8251b8ec1565cf3d9775ccedddec838d56914d2
windrunner9292 <jhleefred@naver.com>
2022-03-16 00:24:13 -0500

    Working on the notification email and comments

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['SQLALCHEMY_TRACN_MODIFICATIONS'] = False
 
 # configs for the email
 app.config['MAIL_SERVER'] = "smtp.mail.yahoo.com"
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = confidential_info[0]
@@ -32,6 +32,7 @@ db = SQLAlchemy(app)
 mail = Mail(app)
 
 class Users(db.Model):
+    # Users table definition
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
@@ -40,6 +41,7 @@ class Users(db.Model):
         return f"User('{self.username}, {self.email}')"
 
 class Queue(db.Model):
+    # Queue table definition
     id = db.Column(db.Integer, primary_key=True)
     firstUser = db.Column(db.String(20), unique=False, nullable=False)
     secondUser = db.Column(db.String(50), unique=False, nullable=False)
@@ -64,20 +66,24 @@ def getCurrentQueue():
     return [(queue.firstUser, queue.secondUser) for queue in Queue.query.all()]
 
 def addUser(username, email):
+    # adds the user to the Users db
     user = Users(username=username, email=email)
     db.session.add(user)
     db.session.commit()
 
 def addQueue(firstUser, secondUser):
+    # adds the row to Queue db
     queue = Queue(firstUser=firstUser, secondUser=secondUser)
     db.session.add(queue)
     db.session.commit()
 
 def deleteQueue(firstUser, secondUser):
+    # delete the entry from the queue
     Queue.query.filter_by(firstUser=firstUser, secondUser=secondUser).delete()
     db.session.commit()
 
 def sendConfirmationEmail(username, email):
+    # used for the confirmation email.
     token = s.dumps(email, salt='email-confirmed')
     msg = Message('Confirm Email', sender=app.config['MAIL_USERNAME'], recipients=[email])
     link = url_for('confirmEmail', token=token, _external=True)
@@ -85,17 +91,19 @@ def sendConfirmationEmail(username, email):
     mail.send(msg)
 
 def sendNotificationEmail(iteration):
+    # used for the notification email.
     currentQueue = getCurrentQueue()
     recipients = [Users.query.filter_by(username=currentQueue[iteration][0]).first().email,
                   Users.query.filter_by(username=currentQueue[iteration][1]).first().email]
-    msg = Message('You are up next!', sender=app.config['MAIL_USERNAME'], recipients=[recipients])
+    msg = Message('You are up next!', sender=app.config['MAIL_USERNAME'], recipients=recipients)
     msg.body = 'This is from automated email. You guys are up next.'
     mail.send(msg)
 
+'''Below are the Functions that interact with frontEnd directly'''
+
 @app.route("/")
 def home():
-    # main landing page
-
+    # main landing page; renders the main page if logged in, login page otherwise.
     return render_template("index.html")
 
 @app.route('/login', methods = ['GET','POST'])
@@ -115,7 +123,7 @@ def login():
             flash("Login Successful!")
             return redirect(url_for("main"))
 
-    if request.method == "GET":                                     # when landing on this page using URL, not POST request
+    if request.method == "GET":                                     # when landing on this page using GET request
         if "user" in session:                                       # if there is a active session
             flash("Already logged in!")
             return render_template("main.html")
@@ -138,40 +146,41 @@ def signup():
             sendConfirmationEmail(username, email)
             flash(f"Confirmation email has been sent to {email}!", "info")
             return redirect(url_for("home"))
-        else:
+        else:                                                               # if username or email already exists
             flash(f"This username or email already exists.", "info")
             return render_template("signup.html")
 
-    elif "user" in session and "temporary" not in session:                                                 # if there's an active session, redirect them to the main.
+    elif "user" in session and "temporary" not in session:                  # if there's an active session, redirect them to the main.
             flash("Already logged in!")
             return render_template("main.html")
     else:
-        return render_template("signup.html")                               # when landing on this page using URL, not POST request
+        return render_template("signup.html")                               # when landing on this page using GET request
 
 @app.route('/confirm_email/<token>', methods=['GET','POST'])
 def confirmEmail(token):
     # this function is called when the user clicks on the confirmation email.
-
     try:
-        if "user" in session:
-            email = s.loads(token, salt='email-confirmed', max_age=TokenTimer)
+        if "user" in session:                                                               # if temporary user session is detected, add the user to the db.
+            email = s.loads(token, salt='email-confirmed', max_age=TokenTimer)              # TokenTimer represents the time in seconds.
             addUser(session["user"], session["email"])
             flash("Account has been successfully created for {}!".format(session["user"]), "info")
-            session.pop("user", None)
+            session.pop("user", None)                                                       # then delete the temporary user session info.
             session.pop("email", None)
             session.pop("temporary", None)
         else:
-            flash("The link is unavailble.", "info")
-    except SignatureExpired:                # if the token timer is expired.
+            flash("The link is unavailble.", "info")                                        # if the user clicked on "start over", then this fires.
+    except SignatureExpired:                                                                # if the token timer is expired, redirect to main.
         flash("The token is expired.", "info")
     return redirect(url_for("home"))
 
 @app.route("/redirectToMain", methods=["POST","GET"])
 def redirectToMain():
+    # used for the redirection to prevent unwanted Form submission.
     return redirect(url_for("main"))
 
 @app.route("/showDashboard", methods=["POST","GET"])
 def redirectMainGetRequest():
+    # This handles the GET request for the main page.
     currentUsers = getAllUsers()
     currentQueue = getCurrentQueue()
     return render_template("main.html", 
@@ -185,7 +194,7 @@ def main():
         user = session["user"]
         currentUsers = getAllUsers()
         if request.method == "POST":
-            if request.form['action'] == 'Submit':
+            if request.form['action'] == 'Submit':                                      # button for adding the match to the queue
                 firstPlayer = request.form["firstPlayer"]
                 secondPlayer = request.form["secondPlayer"]
                 addQueue(firstPlayer, secondPlayer)
@@ -193,7 +202,7 @@ def main():
                 return redirect(url_for("main", 
                                         currentUsers=currentUsers,
                                         currentQueue=currentQueue))
-            if request.form['action'] == 'Game over, Notify next players in Queue':
+            if request.form['action'] == 'Game over, Notify next players in Queue':     # button for notifying and deleting the first queue.
                 firstCurrentPlayer = request.form["firstCurrentPlayer"]
                 secondCurrentPlayer = request.form["secondCurrentPlayer"]
                 sendNotificationEmail(0)
@@ -202,7 +211,7 @@ def main():
                 return redirect(url_for("main", 
                                         currentUsers=currentUsers,
                                         currentQueue=currentQueue))
-            if request.form['action'] == 'Game over, Just delete us from the Queue':
+            if request.form['action'] == 'Game over, Just delete us from the Queue':    # button for deleting the first queue.
                 firstCurrentPlayer = request.form["firstCurrentPlayer"]
                 secondCurrentPlayer = request.form["secondCurrentPlayer"]
                 deleteQueue(firstCurrentPlayer,secondCurrentPlayer)
@@ -210,15 +219,14 @@ def main():
                 return redirect(url_for("main", 
                                         currentUsers=currentUsers,
                                         currentQueue=currentQueue))
-            if request.form['action'] == 'Delete':
+            if request.form['action'] == 'Delete':                                      # button for deleting the n'th queue.
                 firstPlayerInQueue = request.form["firstPlayerInQueue"]
                 secondPlayerInQueue = request.form["secondPlayerInQueue"]
                 deleteQueue(firstPlayerInQueue,secondPlayerInQueue)
                 currentQueue = getCurrentQueue()
                 return redirect(url_for("main", 
                                         currentUsers=currentUsers,
-                                        currentQueue=currentQueue))
-                
+                                        currentQueue=currentQueue))        
         if request.method == "GET":
             return redirect(url_for("redirectMainGetRequest"))
     else:

933408d7591bbd3c6feda4cd119ee1c0cc24c942
windrunner9292 <jhleefred@naver.com>
2022-04-26 17:28:46 -0500

    reverting the last revert; the system error was due to heroku service, not my app.

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def getCurrentLeaderBoard():
     rank_users = []
     for user in Users.query.all():
         if user.isParticipatingLeague == 1:
-            rank_users.append((user.username, user.rank))
+            rank_users.append((user.username, user.rank, user.streak_ranked if user.lastResult_ranked == 1 else 0))
     rank_users.sort(key=lambda i:i[1])
     return rank_users
 
